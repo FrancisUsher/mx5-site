2df71f1b292170fd5eaf56c4514c5152bcb3de3a
Francis Usher <francis.w.usher@gmail.com>
2024-05-26 21:00:33 -0700

    add speculative prerendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,11 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://mx5.usher.codes",
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
+  experimental: {
+    clientPrerender: true,
+  },
 });
